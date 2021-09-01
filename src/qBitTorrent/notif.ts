ae00cc5ef6419668c871a67fec1d94602fa2fa07
pyra07 <yousuf5550@gmail.com>
2021-09-01 20:04:36 +0400

    in process: notifs

---
+++ src/qBitTorrent/notif.ts
@@ -0,0 +1,5 @@
+// Activates whenever a torrent has finished downloading.
+// Sends a Discord webhook to the user notifying them that the torrent has finished downloading.
+import {webhook} from "../../profile.json"
+
+console.log(process.argv[2]);
