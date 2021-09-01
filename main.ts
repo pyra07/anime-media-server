6e057156ebe9a3f32b6e185526733c39b884b33f
Yousuf <yousuf5550@gmail.com>
2021-09-01 22:10:00 +0400

    rm annoying links

--- main.ts
+++ main.ts
@@ -3,35 +3,3 @@ import Scheduler from "./src/Scheduler/schedule";
 (async () => {
   await Scheduler.run("0 */45 * * * *");
 })();
-
-// Keep this for reference lol
-const list = [
-  "https://nyaa.si/download/1419352.torrent",
-  "https://nyaa.si/download/1422158.torrent",
-  "https://nyaa.si/download/1420518.torrent",
-  "https://nyaa.si/download/1418661.torrent",
-  "https://nyaa.si/download/1421478.torrent",
-  "https://nyaa.si/download/1384834.torrent",
-  "https://nyaa.si/download/1387772.torrent",
-  "https://nyaa.si/download/1390425.torrent",
-  "https://nyaa.si/download/1393297.torrent",
-  "https://nyaa.si/download/1396042.torrent",
-  "https://nyaa.si/download/1398796.torrent",
-  "https://nyaa.si/download/1401710.torrent",
-  "https://nyaa.si/download/1404253.torrent",
-  "https://nyaa.si/download/1380141.torrent",
-  "https://nyaa.si/download/1383253.torrent",
-  "https://nyaa.si/download/1386101.torrent",
-  "https://nyaa.si/download/1388760.torrent",
-  "https://nyaa.si/download/1391620.torrent",
-  "https://nyaa.si/download/1394481.torrent",
-  "https://nyaa.si/download/1397180.torrent",
-  "https://nyaa.si/download/1400055.torrent",
-  "https://nyaa.si/download/1402783.torrent",
-  "https://nyaa.si/download/1416915.torrent",
-  "https://nyaa.si/download/1419608.torrent",
-  "https://nyaa.si/download/1422408.torrent",
-  "https://nyaa.si/download/1415665.torrent",
-  "https://nyaa.si/download/1418229.torrent",
-  "https://nyaa.si/download/1421057.torrent",
-];
