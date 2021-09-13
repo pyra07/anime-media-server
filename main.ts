50f66c821f6a48c5bdfdd83ce27f6c0b9eb92555
pyra07 <yousuf5550@gmail.com>
2021-09-13 17:30:39 +0400

    check profie.json

--- main.ts
+++ main.ts
@@ -1,5 +1,22 @@
 import Scheduler from "./src/Scheduler/schedule";
+import profile from "./profile.json";
+import anilist from "./src/Anilist/anilist";
+import fs from "fs";
 
 (async () => {
-  await Scheduler.run("0 */10 * * * *");
+  // Check if every key in profile is not null or empty
+  Object.entries(profile).forEach(async ([key, value]) => {
+    if (value === 0 || value === "") {
+      if (key === "id") {
+        const data = await anilist.customQuery(
+          "query ($name : String){ User(name: $name) { id } }",
+          { name: profile.aniUserName }
+        );
+        profile.id = data.data.User.id;
+        fs.writeFileSync("./profile.json", JSON.stringify(profile, null, 2));
+      } else throw new Error(`${key} is not defined in profile.json`);
+    }
+  });
+
+  await Scheduler.check2();
 })();
