3df36b1ce48c336409eca89481bab132c72d6314
pyra07 <yousuf5550@gmail.com>
2021-09-15 18:32:14 +0400

    better schedule.ts i hope

--- src/Scheduler/schedule.ts
+++ src/Scheduler/schedule.ts
@@ -10,27 +10,19 @@ import { AniQuery } from "../utils/types";
 class Scheduler {
   constructor() {}
 
-  private getDifferences(animeList: any[], fireDBList: any[]) {
+  private getDifferences(animeList: AniQuery[], fireDBList: any[]) {
     // Go through both lists. If key mediaId does not exist in firedblist, add it to animeList
     // If key mediaId does exist in firedblist, check if the value is different. If it is different, add it to animeList
-
-    var animeListCopy = animeList.slice(0);
-    var fireDBListCopy = fireDBList.slice(0);
-    for (var i = 0; i < fireDBListCopy.length; i++) {
-      var fireDBItem = fireDBListCopy[i];
-      var animeItem = animeListCopy.find(
-        (item) => item.mediaId === fireDBItem.mediaId
+    const differences: AniQuery[] = [];
+    animeList.forEach((anime) => {
+      const fireDBAnime = fireDBList.find(
+        (item) => item.mediaId === anime.mediaId
       );
-      if (animeItem) {
-        if (animeItem.status !== fireDBItem.status) {
-          animeListCopy.push(fireDBItem);
-        }
-        animeListCopy.splice(animeListCopy.indexOf(animeItem), 1);
-      } else {
-        animeListCopy.push(fireDBItem);
+      if (!fireDBAnime) {
+        differences.push(anime);
       }
-    }
-    return animeListCopy;
+    });
+    return differences;
   }
 
   /**
@@ -43,7 +35,7 @@ class Scheduler {
       async () => {
         // log with current time
         console.log("Checking", new Date().toLocaleTimeString());
-        await this.check();
+        await this.check2();
       },
       null,
       true,
@@ -51,93 +43,194 @@ class Scheduler {
     );
   }
 
-  /**
-   * Checks for new animes, new episodes, and downloads them. wow
-   */
-  public async check() {
-    const animeDb : AniQuery[] = await Anilist.getAnimeUserList();
-    let fireDB = await DB.getFromDb();
-
-    if (!fireDB) {
-      console.log("No firebase data found. Creating new one");
-      await DB.addToDb(animeDb);
-      fireDB = await DB.getFromDb();
+  private async handleAnime(
+    animeDb: AniQuery[],
+    fireDBData?: firebase.firestore.DocumentData[]
+  ) {
+    // Check if fireDBData is empty
+    if (fireDBData === undefined) {
+      fireDBData = animeDb as firebase.firestore.DocumentData[];
     }
 
-    const fireDBData = fireDB.docs.map((doc) => doc.data()); // convert to array
-
-    // If the user add a new anime, add it to the firebase database
+    // Check if user has added new anime. If so, add it to firebase
     const listDifferences = this.getDifferences(animeDb, fireDBData);
-    if (listDifferences) await DB.addToDb(listDifferences);
+    if (listDifferences.length > 0) await DB.addToDb(listDifferences);
     fireDBData.push(...listDifferences);
 
-    // Loops through anime list
+    // Go thru each anime
     for (let index = 0; index < animeDb.length; index++) {
       const anime = animeDb[index];
       const fireDBAnime = fireDBData.find(
         (item) => item.mediaId === anime.mediaId
       );
+      // Should find fail, just skip.
       if (!fireDBAnime) continue;
 
-      // Takes whichever user progress is the latest
+      // Users progress
       const startEpisode = anime.progress;
-      // anime.progress > fireDBAnime.progress
-      //   ? anime.progress
-      //   : fireDBAnime.progress;
 
       // NextAiringEpisode can be null if the anime is finished. So check for that
       const endEpisode = anime.media.nextAiringEpisode
         ? anime.media.nextAiringEpisode.episode - 1
         : anime.media.episodes;
 
-      const fsDownloadedEpisodes: number[] =
-        fireDBAnime.downloadedEpisodes || [];
-
+      // If progress is up to date, then skip
       if (startEpisode === endEpisode) continue;
 
-      const downloadList = await Nyaa.getTorrents(
-        [anime],
+      // Firestore downloaded episodes
+      const fsDownloadedEpisodes = fireDBAnime.downloadedEpisodes || [];
+
+      const torrents = await Nyaa.getTorrents(
+        anime,
         startEpisode,
         endEpisode,
         fsDownloadedEpisodes
       );
 
-      if (downloadList.length > 0) {
-        downloadList.forEach((torrent) => {
-          console.log(
-            "Downloading :",
-            torrent.title,
-            torrent.link,
-            torrent.episode
-          );
-        });
-
-        // Individually download each episode
-        let epDownloadedList: number[] = [];
-        try {
-          for (let i = 0; i < downloadList.length; i++) {
-            const torrent = downloadList[i];
-            const isAdded = await qbit.addTorrent(
-              torrent.link,
-              anime.media.title.romaji
-            );
-            if (isAdded) epDownloadedList.push(parseInt(torrent.episode));
-            // Wait for 1 second. Avoids torrents not being added
-            await new Promise((resolve) => setTimeout(resolve, 1000));
-          }
-          epDownloadedList.push(...fsDownloadedEpisodes);
-          await DB.updateProgress(
-            anime.mediaId.toString(),
-            parseInt(downloadList[downloadList.length - 1].episode),
-            anime.media,
-            epDownloadedList
-          );
-        } catch (error) {
-          console.log(error);
+      // Check if torrents are empty
+      if (Array.isArray(torrents)) {
+        if (torrents.length === 0) continue;
+        for (let index = 0; index < torrents.length; index++) {
+          const torrent = torrents[index];
+          console.log("Downloading", torrent.title, torrent.link);
+          await qbit.addTorrent(torrent.link, anime.media.title.romaji);
+          // Wait for 500ms to prevent qbit from crashing
+          await new Promise((resolve) => setTimeout(resolve, 500));
         }
+
+        fsDownloadedEpisodes.push(
+          ...torrents.map((torrent) => parseInt(torrent.episode))
+        );
+
+        await DB.updateProgress(
+          anime.mediaId.toString(),
+          parseInt(torrents[torrents.length - 1].episode),
+          anime.media,
+          fsDownloadedEpisodes
+        );
+      } else {
+        if (Object.keys(torrents).length === 0) continue;
+        console.log("Downloading Batch", torrents.title, torrents.link);
+        await qbit.addTorrent(torrents.link, anime.media.title.romaji);
+        // Generate array of numbers between start and end
+        const episodeArray = Array.from(
+          { length: endEpisode - startEpisode },
+          (v, k) => k + startEpisode + 1
+        );
+        await DB.updateProgress(
+          anime.mediaId.toString(),
+          0,
+          anime.media,
+          episodeArray
+        );
+        
+
+        
+
       }
     }
   }
+
+  public async check2() {
+    const animeDb: AniQuery[] = await Anilist.getAnimeUserList();
+    const fireDB = await DB.getFromDb();
+
+    if (fireDB.docs.length === 0) {
+      console.log("No firebase data found. Creating new one");
+      await DB.addToDb(animeDb);
+      await this.handleAnime(animeDb);
+    } else {
+      const fireDBData = fireDB.docs.map((doc) => doc.data()); // convert to array
+      await this.handleAnime(animeDb, fireDBData);
+    }
+  }
+
+  /**
+   * Checks for new animes, new episodes, and downloads them. wow
+   */
+  // public async check() {
+  //   const animeDb: AniQuery[] = await Anilist.getAnimeUserList();
+  //   let fireDB = await DB.getFromDb();
+
+  //   if (!fireDB) {
+  //     console.log("No firebase data found. Creating new one");
+  //     await DB.addToDb(animeDb);
+  //     fireDB = await DB.getFromDb();
+  //   }
+
+  //   const fireDBData = fireDB.docs.map((doc) => doc.data()); // convert to array
+  //   // If the user add a new anime, add it to the firebase database
+  //   const listDifferences = this.getDifferences(animeDb, fireDBData);
+  //   if (listDifferences) await DB.addToDb(listDifferences);
+  //   fireDBData.push(...listDifferences);
+
+  //   // Loops through anime list
+  //   for (let index = 0; index < animeDb.length; index++) {
+  //     const anime = animeDb[index];
+  //     const fireDBAnime = fireDBData.find(
+  //       (item) => item.mediaId === anime.mediaId
+  //     );
+  //     if (!fireDBAnime) continue;
+
+  //     // Takes whichever user progress is the latest
+  //     const startEpisode = anime.progress;
+  //     // anime.progress > fireDBAnime.progress
+  //     //   ? anime.progress
+  //     //   : fireDBAnime.progress;
+
+  //     // NextAiringEpisode can be null if the anime is finished. So check for that
+  //     const endEpisode = anime.media.nextAiringEpisode
+  //       ? anime.media.nextAiringEpisode.episode - 1
+  //       : anime.media.episodes;
+
+  //     const fsDownloadedEpisodes: number[] =
+  //       fireDBAnime.downloadedEpisodes || [];
+
+  //     if (startEpisode === endEpisode) continue;
+
+  //     const downloadList = await Nyaa.getTorrents(
+  //       anime,
+  //       startEpisode,
+  //       endEpisode,
+  //       fsDownloadedEpisodes
+  //     );
+
+  //     if (downloadList.length > 0) {
+  //       downloadList.forEach((torrent) => {
+  //         console.log(
+  //           "Downloading :",
+  //           torrent.title,
+  //           torrent.link,
+  //           torrent.episode
+  //         );
+  //       });
+
+  //       // Individually download each episode
+  //       let epDownloadedList: number[] = [];
+  //       try {
+  //         for (let i = 0; i < downloadList.length; i++) {
+  //           const torrent = downloadList[i];
+  //           const isAdded = await qbit.addTorrent(
+  //             torrent.link,
+  //             anime.media.title.romaji
+  //           );
+  //           if (isAdded) epDownloadedList.push(parseInt(torrent.episode));
+  //           // Wait for 1 second. Avoids torrents not being added
+  //           await new Promise((resolve) => setTimeout(resolve, 1000));
+  //         }
+  //         epDownloadedList.push(...fsDownloadedEpisodes);
+  //         await DB.updateProgress(
+  //           anime.mediaId.toString(),
+  //           parseInt(downloadList[downloadList.length - 1].episode),
+  //           anime.media,
+  //           epDownloadedList
+  //         );
+  //       } catch (error) {
+  //         console.log(error);
+  //       }
+  //     }
+  //   }
+  // }
 }
 
 export default new Scheduler();
