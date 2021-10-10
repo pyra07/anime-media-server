2ce10e02cad128d90fe2ccfcdd983c0de53383f0
pyra07 <yousuf5550@gmail.com>
2021-10-10 21:20:21 +0400

    cleanup main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,5 @@
-import profile from "./profile.json";
-import anilist from "./src/Anilist/anilist";
-import fs from "fs";
 import ui from "./src/UI/ui";
 
-(async () => {
-  // Check if every key in profile is not null or empty
-  Object.entries(profile).forEach(async ([key, value]) => {
-    if (value === 0 || value === "") {
-      if (key === "id") {
-        const data = await anilist.customQuery(
-          "query ($name : String){ User(name: $name) { id } }",
-          { name: profile.aniUserName }
-        );
-        profile.id = data.data.User.id;
-        fs.writeFileSync("./profile.json", JSON.stringify(profile, null, 2));
-      } else throw new Error(`${key} is not defined in profile.json`);
-    }
-  });
+const arg = process.argv.pop();
 
-  ui.init();
-})();
+ui.init(arg);
