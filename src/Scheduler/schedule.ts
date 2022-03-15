6bf17a81b264fcdb9cc0881f2d11269b99ce1b78
Yousuf <yousuf5550@gmail.com>
2022-03-15 16:08:39 +0400

    typo

--- src/Scheduler/schedule.ts
+++ src/Scheduler/schedule.ts
@@ -40,7 +40,7 @@ class Scheduler {
       async () => {
         // log with current time
         log(
-          `Running scheduler at ${new Date().toLocaleString()} - ${new Date().toLocaleDateString()}`
+          `Running scheduler at ${new Date().toLocaleString()}}`
         );
         await this.check();
       },
