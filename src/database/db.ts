43d7e8b6698b4a8d90e37248d35b4c75d1087fc1
pyra07 <yousuf5550@gmail.com>
2022-03-17 22:45:18 +0400

    nope

--- src/database/db.ts
+++ src/database/db.ts
@@ -12,7 +12,6 @@ class DB {
   }
 
   public async logIn() {
-    await this.myProject.firestore().enablePersistence();
     DB.user = await this.myProject
       .auth()
       .signInWithEmailAndPassword(email, emailPassword);
