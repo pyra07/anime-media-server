a299dbfd5f2f975606a44c48bfef47ba54a14a46
pyra07 <yousuf5550@gmail.com>
2022-05-14 01:31:05 +0400

    ye

--- src/Scheduler/schedule.ts
+++ src/Scheduler/schedule.ts
@@ -146,6 +146,36 @@ class Scheduler {
       fsDownloadedEpisodes.length === endEpisode - startEpisode || // you are up to date (but not all episodes need to be downloaded)
       fsDownloadedEpisodes.length === anime.media.episodes; // You have downloaded all episodes
 
+    log(`${anime.media.title.romaji} is up to date`);
+    log(`${startEpisode === endEpisode} -- startEpisode === endEpisode`);
+    log(`${anime.progress >= endEpisode} -- anime.progress >= endEpisode`);
+    log(
+      `${
+        anime.progress === anime.media.episodes
+      } -- anime.progress === anime.media.episodes`
+    );
+    log(
+      `${
+        fsDownloadedEpisodes.length === anime.progress
+      } -- fsDownloadedEpisodes.length === anime.progress`
+    );
+    log(
+      `${
+        fsDownloadedEpisodes.length === endEpisode
+      } -- fsDownloadedEpisodes.length === endEpisode`
+    );
+    log(
+      `${
+        fsDownloadedEpisodes.length === endEpisode - startEpisode
+      } -- fsDownloadedEpisodes.length === endEpisode - startEpisode`
+    );
+    log(
+      `${
+        fsDownloadedEpisodes.length === anime.media.episodes
+      } -- fsDownloadedEpisodes.length === anime.media.episodes`
+    );
+    log();
+
     if (isUpToDate) return;
 
     // Attempt to find the anime.
