43e9aea2b79f456a0b647da33096552a3cd9b9dd
pyra07 <yousuf5550@gmail.com>
2022-05-13 20:40:01 +0400

    ll

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
 import ui from "./src/UI/ui";
 
 const arg = process.argv.pop();
-
 ui.init(arg);
--- src/Scheduler/schedule.ts
+++ src/Scheduler/schedule.ts
@@ -100,7 +100,7 @@ class Scheduler {
   private async handleAnime(anime: AniQuery): Promise<void> {
     log(`Handling ${anime.media.title.romaji}`);
     try {
-      const fireDBEntry = await DB.getByMediaId(`${123}`);
+      const fireDBEntry = await DB.getByMediaId(`${anime.mediaId}`);
 
       if (!fireDBEntry.exists) {
         DB.addToDb(anime);
