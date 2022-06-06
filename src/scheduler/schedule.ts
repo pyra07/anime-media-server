75252764c72b081b973b13a5c1dc4eb43736d123
Yousuf <yousuf5550@gmail.com>
2022-06-06 19:32:27 +0400

    i hate console

--- src/scheduler/schedule.ts
+++ src/scheduler/schedule.ts
@@ -7,7 +7,6 @@ import { joinArr } from "@scheduler/utils";
 import { AnimeTorrent, AniQuery, OfflineAnime, OfflineDB } from "@utils/index";
 import { MessageBuilder, Webhook } from "discord-webhook-node";
 import { webhook } from "profile.json";
-import { log } from "console";
 import { arrayUnion } from "firebase/firestore";
 
 class Scheduler {
@@ -28,7 +27,7 @@ class Scheduler {
     const job = new cron.CronJob(
       cronTime,
       async () => {
-        log(`Running scheduler at ${new Date().toLocaleString()}`); // log with current time
+        console.log(`Running scheduler at ${new Date().toLocaleString()}`); // log with current time
         await this.check();
       },
       null,
@@ -39,7 +38,7 @@ class Scheduler {
       new cron.CronJob(
         "0 0 */1 * *",
         () => {
-          log(`Clearing offlineDB at ${new Date().toLocaleString()}`); // log with current time
+          console.log(`Clearing offlineDB at ${new Date().toLocaleString()}`); // log with current time
           this.clearOfflineDB();
         },
         null,
@@ -72,7 +71,9 @@ class Scheduler {
   ): Promise<void> {
     const downloadedEpisodes = new Array<number>();
     for (const torrent of animeTorrent) {
-      log(`Downloading ${torrent.title} ${torrent.episode} at ${torrent.link}`);
+      console.log(
+        `Downloading ${torrent.title} ${torrent.episode} at ${torrent.link}`
+      );
       // Download torrent
       var isAdded: boolean = await qbit.addTorrent(
         torrent.link,
@@ -124,7 +125,7 @@ class Scheduler {
    * @returns Promise
    */
   private async handleAnime(anime: AniQuery): Promise<void> {
-    log(`Handling ${anime.media.title.romaji}`);
+    console.log(`Handling ${anime.media.title.romaji}`);
     let fireDBAnime;
 
     try {
@@ -201,6 +202,7 @@ class Scheduler {
       const synonyms = [anime.media.title.english, ...anime.media.synonyms];
       for (const synonym of synonyms) {
         // TODO If synonym is not in English, skip
+        console.log(`Handling ${synonym}`);
         anime.media.title.romaji = synonym;
         const isValidTitle = await this.getTorrents(
           anime,
@@ -243,7 +245,7 @@ class Scheduler {
     // If we found no torrents, then set a timeout to offlineDB
     if (torrents === null) {
       this.offlineAnimeDB[anime.mediaId].setTimeout();
-      log(`No torrents found for ${anime.media.title.romaji}`);
+      console.log(`No torrents found for ${anime.media.title.romaji}`);
       return false; // Guard against null torrents (error | not found)
     }
 
