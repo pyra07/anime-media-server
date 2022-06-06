d1ae9e3bc110c0eadc3a63b1910ed6c68386e9ef
Yousuf <yousuf5550@gmail.com>
2022-06-07 02:06:54 +0400

    early implementation of getAnimeSeason

--- src/scheduler/utils.ts
+++ src/scheduler/utils.ts
@@ -14,20 +14,26 @@ function joinArr(array: any[]) {
  */
 function getAnimeSeason(animeEntry: string): number {
   /* Try to infer the season from the anime title
+     
    *  Example:
    * - Case 1 : Boku no Hero Academia             --> Season 1
-   * - Case 2 : Boku no Hero Academia 4           --> Season 4
-   * - Case 3 : Boku no Hero Academia S4          --> Season 4
-   * - Case 4 : Boku no Hero Academia Season 4    --> Season 4
-   * - Case 5 : Boku no Hero Academia Season IV    --> Season 4
-   * - Case 5 : Boku no Hero Academia 4th Season  --> Season 4
+   * - Case 2 : Boku no Hero Academia 3           --> Season 3
+   * - Case 3 : Boku no Hero Academia S2          --> Season 2
+   * - Case 4 : Boku no Hero Academia Season 5    --> Season 5
+   * - Case 5 : Boku no Hero Academia Season IV   --> Season 4 (optional case)
+   * - Case 6 : Boku no Hero Academia 7th Season  --> Season 7 (s7 doesn't exist but who cares)
    */
 
-  // Case 2
-  const regex2 = /^(.*) (\d+)$/;
+  const seasonRegex =
+    /s\d{1}|season(.*)\d{1}|(\d+(st|nd|rd|th)(.*)season)|[^a-zA-Z0-9]\d{1}$/i;
+  const seasonString = animeEntry.match(seasonRegex);
+  // If found, extract the season number
+  if (seasonString) {
+    const seasonNumber = seasonString[0].match(/\d+/);
+    return seasonNumber ? parseInt(seasonNumber[0]) : 0;
+  }
 
-  // TODO : Unimplemented
-  return 0;
+  return 1;
 }
 
-export { joinArr };
+export { joinArr, getAnimeSeason };
