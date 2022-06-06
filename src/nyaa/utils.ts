0ac00cb13af8852b36069ac62d2693ec7faf3de2
pyra07 <yousuf5550@gmail.com>
2022-06-06 05:06:22 +0400

    moved method here

--- src/nyaa/utils.ts
+++ src/nyaa/utils.ts
@@ -1,3 +1,15 @@
+import Parser from "rss-parser";
+import {
+  AnimeFormat,
+  AnimeTorrent,
+  AniQuery,
+  Resolution,
+  SearchMode,
+} from "@utils/index";
+import { resolution } from "profile.json";
+import { findBestMatch } from "string-similarity";
+import anitomy from "anitomy-js";
+
 /**
  * Gets the numbers between start and end
  * @param  {number} start
@@ -5,12 +17,120 @@
  * @returns {number[]}
  */
 
-export function getNumbers(start: number, inBetween: number[], end: number): number[] {
+function getNumbers(start: number, inBetween: number[], end: number): number[] {
   let numbers = [];
   for (let i = start + 1; i <= end; i++) {
     if (inBetween.indexOf(i) === -1) numbers.push(i);
   }
   return numbers;
 }
+/**
+ * Similarity check for episode range for batch torrents
+ * @param  {string} episode The episode number to verify
+ * @param  {RegExpMatchArray} episodeRange
+ * @returns boolean
+ */
+function verifyEpisodeRange(
+  episode: string,
+  episodeRange: RegExpMatchArray
+): boolean {
+  // If the file name contains a range of episodes, check if the episode is in the range
+  // If so we can assume the torrent is a batch as well.
+  const e = episodeRange[0].split(/[-~]/);
+  const myE = episode.split("-");
+  if (
+    parseInt(e[0]) === parseInt(myE[0]) &&
+    parseInt(e[1]) === parseInt(myE[1])
+  )
+    return true; // If the range is similar, return true
+  else return false; // If the range is not similar, return false
+}
 
+/**
+ * Verifies if the query has some degree of similarity to the media to look for, based on the input given.
+ * @param  {string} searchQuery The title of the anime to originally verify
+ * @param  {anitomy.AnitomyResult} animeParsedData The parsed data of the RSS item title
+ * @param  {Resolution} resolution The resolution to verify
+ * @param  {SearchMode} searchMode What to expect from the query. This can be in multiple forms
+ * @param  {string} episode? The episode number to verify, if applicable
+ * @returns {boolean} Returns true if the query is similar to the media we want, otherwise returns false
+ */
+function verifyQuery(
+  searchQuery: string,
+  animeParsedData: anitomy.AnitomyResult,
+  resolution: Resolution,
+  searchMode: SearchMode,
+  episode?: string
+): boolean {
+  const fileName = animeParsedData.file_name;
+  const parsedTitle = animeParsedData.anime_title;
+  const parsedResolution = animeParsedData.video_resolution;
 
+  if (!parsedTitle || !parsedResolution) return false; // Guard against empty parsed data
+
+  // If parsedTitle has a title in round brackets, extract it. If found, extract the title out of the brackets
+  const subAnimeTitle = parsedTitle.match(/(?<=\().+?(?=\))/);
+  const subAnimeTitleString = subAnimeTitle ? subAnimeTitle[0] : "";
+  const mainAnimeTitle = parsedTitle.replace(/\(.+?\)/, "");
+
+  // if animeTitle is seperated by a '|', then split this.
+  const vBarSplitTitle = parsedTitle.split("|");
+
+  /**Attempt to find best match based on various titles procured
+   * Using lowercase to avoid tampering with bestMatch rating (not important)
+   */
+  const titleMatch = findBestMatch(searchQuery.toLowerCase(), [
+    parsedTitle.toLowerCase(),
+    mainAnimeTitle.toLowerCase(),
+    subAnimeTitleString.toLowerCase(),
+    ...vBarSplitTitle.map((x) => x.toLowerCase()),
+  ]);
+
+  const resolutionMatch = parsedResolution.includes(resolution);
+
+  // If title is not similar, and resolution is not similar, return false
+  if (titleMatch.bestMatch.rating < 0.8) return false;
+  if (!resolutionMatch) return false;
+
+  switch (searchMode) {
+    case SearchMode.EPISODE:
+      const parsedEpisode = animeParsedData.episode_number;
+      if (!parsedEpisode) return false; // Guard against empty episode
+
+      const episodeMatch = parseInt(episode!) === parseInt(parsedEpisode); // Check if episode is similar
+
+      return episodeMatch; // Return if all conditions are met
+
+    case SearchMode.BATCH:
+      const parsedReleaseInfo = animeParsedData.release_information;
+
+      const batchMatch = parsedReleaseInfo?.includes("Batch"); // Check if it is a batch
+
+      /* Usually some batches don't explicitly specify that the torrent itself is a
+         batch. This can be combated by proving there is no episode number to be parsed
+         Therefore we assume this is a batch (to be tested further) */
+      const isEpisode = animeParsedData.episode_number;
+      "".match(/\d+/g);
+
+      const episodeRange = fileName.match(/\d+( *)[-~]( *)\d+/); // Check if the file name contains a range of episodes
+      if (episodeRange) return verifyEpisodeRange(episode!, episodeRange); // If so, check if the episode is in the range
+
+      return !!(batchMatch || !isEpisode); // Return if all conditions are met.
+
+    // For these following cases, they are only dependent on the title, and resolution.
+    case SearchMode.MOVIE:
+    case SearchMode.TV_SHORT:
+      return true;
+
+    case SearchMode.OVA:
+    case SearchMode.ONA:
+      const oEpisodeRange = fileName.match(/\d+( *)[-~]( *)\d+/); // Check if the file name contains a range of episodes
+      if (oEpisodeRange) return verifyEpisodeRange(episode!, oEpisodeRange); // If so, check if the episode is in the range
+      return true;
+
+    default:
+      return false;
+  }
+}
+
+export { getNumbers, verifyEpisodeRange, verifyQuery };
