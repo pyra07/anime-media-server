12e12c81029387f12561edf79c881e838882fbc0
Yousuf <yousuf5550@gmail.com>
2022-06-08 00:08:20 +0400

    comments

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 # Used to run animu with pm2
 # This makes it so that the script can be restarted easily
 # in case of a crash
+# This runs the scheduler alongside discord bot
 pm2 start ts-node -- --type-check -r tsconfig-paths/register src/main.ts 3
