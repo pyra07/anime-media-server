40a1af3c62ceb902607ee0b2f4f06e00e4597343
ya27hw <yousuf5550@gmail.com>
2022-07-05 19:00:28 +0400

    added watch

--- script.sh
+++ script.sh
@@ -2,4 +2,4 @@
 # This makes it so that the script can be restarted easily
 # in case of a crash
 # This runs the scheduler alongside discord bot
-pm2 start ts-node -- --type-check -r tsconfig-paths/register src/main.ts 3
+pm2 start ts-node -- --type-check -r tsconfig-paths/register src/main.ts 3 --watch
