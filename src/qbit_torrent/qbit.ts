4a691fde333302b80fcedbeb9790bd8295028b6f
ya27hw <yousuf5550@gmail.com>
2023-12-09 14:10:55 +0400

    use own qbit api

--- src/qbit_torrent/qbit.ts
+++ src/qbit_torrent/qbit.ts
@@ -1,35 +1,71 @@
-import { QBittorrent } from "@ctrl/qbittorrent";
+import axios from "axios";
 import path from "path/posix";
 import { torrent_url, password, username, rootDir } from "profile.json";
 
 class QbitTorrent {
-  private client: QBittorrent;
+  private sid?: string;
 
-  constructor() {
-    this.client = new QBittorrent({
-      baseUrl: torrent_url,
-      username: username,
-      password: password,
-    });
+  // Function to authenticate and get the SID (Session ID)
+  private async authenticate() {
+    try {
+      const response = await axios.post(
+        `${torrent_url}/api/v2/auth/login`,
+        `username=${encodeURIComponent(username)}&password=${encodeURIComponent(
+          password
+        )}`,
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      );
+
+      const cookie = response.headers["set-cookie"];
+      if (cookie) {
+        const sidMatch = /SID=([^;]+)/.exec(cookie[0]);
+        this.sid = sidMatch ? sidMatch[1] : undefined;
+      } else {
+        console.error("Authentication failed. No session cookie received.");
+      }
+    } catch (error) {
+      console.error("Error during authentication:", error);
+    }
   }
 
+  // Function to add a torrent using the obtained SID
   public async addTorrent(
     link: string,
     title: string,
     episodeStr?: string
   ): Promise<boolean> {
-    /* *sigh* this is so qbit can download the torrent file, without 
-    the need for a VPN. Mirrors FTW */
-    link = link.replace("nyaa.si","nyaa.land");
-    console.log(link);
-    await this.client.login();    
-    
-    const isAdded = await this.client.addMagnet(link, {
-      savepath: path.join(rootDir, title),
-      rename: episodeStr ? `${title} - ${episodeStr}` : title,
-    });
+    await this.authenticate();
+    if (!this.sid) return false;
+    link = link.replace("nyaa.si", "nyaa.land");
 
-    return isAdded;
+    try {
+      const response = await axios.post(
+        `${torrent_url}/api/v2/torrents/add`,
+        `urls=${encodeURIComponent(link)}&savepath=${encodeURIComponent(
+          path.join(rootDir, title)
+        )}&rename=${encodeURIComponent(
+          episodeStr ? `${title} - ${episodeStr}` : title
+        )}`,
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            Cookie: `SID=${this.sid}`,
+          },
+        }
+      );
+
+      if (response.data === "Ok.") {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      return false;
+    }
   }
 }
 
